8905629e8bbbf916ff98873efa871626eb9ad365
Lalit Gour <lgour@rightpoint.com>
2025-05-14 16:37:30 +0530

    Added css and js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -132,8 +132,20 @@ export default async function decorate(block) {
     brandLink.closest('.button-container').className = '';
   }
 
+  const logo = navBrand.querySelector('picture');
+
+  if (logo) {
+    // Replace the first section's contents with the authored image wrapped with a link to '/'
+    navBrand.innerHTML = `<a href="/" aria-label="Home" title="Home" class="home">${logo.outerHTML}</a>`;
+    // Make sure the logo is not lazy loaded as it's above the fold and can affect page load speed
+    navBrand.querySelector('img').settAttribute('loading', 'eager');
+  }
+
   const navSections = nav.querySelector('.nav-sections');
   if (navSections) {
+     navSections.querySelectorAll('.button-container, .button').forEach((button) => {
+      button.classList = '';
+    });
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
       navSection.addEventListener('click', () => {
